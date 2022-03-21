489d0d7edab0d61c01a0804a88c818c69e39a770
kk-1onewolf <kunalkumar2100k@gmail.com>
2022-03-21 23:39:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,6 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import { Component } from "react";
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      restroClickedFlag: false,
-    };
-  }
   render() {
     return (
       <div className="App">
